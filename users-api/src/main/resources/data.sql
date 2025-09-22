16905717b46119b6e0c38714a599cb310f071b02
Davide Flamini Cazaran <davidecazaran@gmail.com>
2025-09-22 18:41:32 -0500

    fix(db): todo-controller fix

--- users-api/src/main/resources/data.sql
+++ users-api/src/main/resources/data.sql
@@ -1,4 +1,6 @@
-INSERT INTO users (username, firstname, lastname, role) VALUES
+INSERT INTO
+  users (username, firstname, lastname, role)
+VALUES
   ('admin', 'Foo', 'Bar', 1),
   ('johnd', 'John', 'Doe', 0),
-  ('janed', 'Jane', 'Doe', 0);
+  ('janed', 'Jane', 'Doe', 0) ON CONFLICT (username) DO NOTHING;
